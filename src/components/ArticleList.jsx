4300e5dd0af8aece64195dc40d479ea9238d626f
Mahmoud <kassah.webdev@gmail.com>
2021-07-26 16:47:19 +0300

    Done.

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Article from './Article'
+
+const ArticleList = ({posts}) => {
+    const articles = posts.map(post => <Article key={post.id} {...post}/>)
+    return (
+        <main>
+            {articles}
+        </main>
+    )
+}
+
+export default ArticleList
